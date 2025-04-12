6f35430ea8922b91bd4822499b185b8c59829a5f
Rodrigo <rodrigopba1@gmail.com>
2025-04-12 16:51:26 -0300

    Refactor code structure for improved readability and maintainability

--- evaluation/fhevm-hardhat-template/transaction-items.js
+++ evaluation/fhevm-hardhat-template/transaction-items.js
@@ -18,7 +18,7 @@ const provider_csv_label = "sepolia"; // Label for CSV filename
 const privateKey = process.env.PRIVATE_KEY || ''; // IMPORTANT: Use env variable or secure method
 //const contractAddress = "0x57e80b81638fc630ad20d614b30cb54c5f879b62"; // Your updated contract address
 //const contractAddress = "0x2158a12c7bf203cb54c041111fa990d402bd6067"; // Your updated contract address
-const contractAddress = "0x73cecc763a6f99ed5a3c40098ef121eae7f7059f"; // Your updated contract address
+const contractAddress = "0xc573701ea03b7f2db93336cefdc1d8bb3bbd9507"; // Your updated contract address
 
 // Helper function to get CPU usage (keep if needed)
 function getCpuUsage() {
@@ -55,7 +55,7 @@ async function main() {
     let abi;
     try {
         // Adjust the path if your artifacts are elsewhere
-        const contractJsonPath = "./artifacts/contracts/add_QoEEvaluatorITEMS.sol/add_QoEEvaluatorITEMS.json";
+        const contractJsonPath = "./artifacts/contracts/sub_QoEEvaluatorITEMS.sol/Subtracting_QoEEvaluatorITEMS.json";
         const contractJson = JSON.parse(fs.readFileSync(contractJsonPath, "utf8"));
         abi = contractJson.abi;
     } catch (err) {
@@ -65,7 +65,7 @@ async function main() {
     const contract = new web3.eth.Contract(abi, contractAddress);
 
     // --- CSV Writer Setup ---
-    const outputFilePath = `performance_ITEMS_metrics_client_PLAINTEXT_${provider_csv_label}_Add.csv`;
+    const outputFilePath = `performance_ITEMS_metrics_client_PLAINTEXT_${provider_csv_label}_All.csv`;
     const fileExists = fs.existsSync(outputFilePath);
     
     const csvWriter = createCsvWriter({
@@ -93,7 +93,7 @@ async function main() {
         .on("data", (data) => results.push(data))
         .on("end", async () => {
             console.log(`Starting to process ${results.length} rows from ${csvFilePath}...`);
-            for (let i = 0; i < 101; i++) {
+            for (let i = 0; i < 81; i++) {
                 const row = results[i];
                 console.log(`\nProcessing row ${i + 1}/${results.length}`);
 
@@ -142,7 +142,7 @@ async function main() {
 
                 try {
                     // --- Create Transaction Payload (Plaintext) ---
-                    const method = contract.methods.addData(
+                    const method = contract.methods.subtractData(
                         qosTypeValue,
                         qodModelValue,
                         qodOSVersionValue,
